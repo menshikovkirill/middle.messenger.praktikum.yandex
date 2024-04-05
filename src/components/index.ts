8b993b61a163cabfad1472272ae2b7b9e503fa94
Menshikov Kirill <menshikov.kirill2604@yandex.ru>
2024-04-06 00:57:43 +0300

    Основная конва и страницы с формами

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './button';
+export { Input } from './input';
+export { InputContainer } from './input-container';
+export { Form } from './form';
+export { Page } from './page';
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { LoginPage } from './login';
+export { NavigatePage } from './navigate';
+export { RegistrationPage } from './registration';
